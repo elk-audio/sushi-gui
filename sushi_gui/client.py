fba2aca4295782551c8b4838769adf48caaa62f2
max <max@elk.audio>
2023-05-16 16:01:23 +0200

    reverted SUSHI_ADDRESS to localhost

--- sushi_gui/client.py
+++ sushi_gui/client.py
@@ -6,8 +6,7 @@ from main_window import MainWindow
 
 
 # If sushi is running on another device replace 'localhost' with the ip of that device 
-# SUSHI_ADDRESS = 'localhost:51051'
-SUSHI_ADDRESS = '192.168.1.108:51051'
+SUSHI_ADDRESS = 'localhost:51051'
 
 
 def main():
